18f6c3708540c973645ad40b8449b8fad81a507d
Kübra Yeşilkaya <kubrayesilkaya.ky@gmail.com>
2023-08-27 19:23:13 +0300

    Sınıf Nesneleri 3

---
+++ Sınıf Nesneleri 3/Sınıf Nesneleri 3/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sınıf_Nesneleri_3
+{
+    internal class Account
+    {
+        public Account()
+        {
+
+        }
+
+        public string GetAccount(string userName)
+        {
+            if(!string.IsNullOrEmpty(userName))
+            {
+
+            }
+            
+            if(!string.IsNullOrWhiteSpace(userName)
+            {
+
+            }
+
+            return "DİJİBİL";
+        }
+        public string GetAccount(int? userId)
+        {
+
+            string result = string.Empty;
+
+            if (userId > 0)
+            {
+                //...
+                result = "Cihan Özhan";
+            }
+            else
+            {
+                throw new Exception("Hata");
+            }
+            return result;
+        }
+    }
+}
---
+++ Sınıf Nesneleri 3/Sınıf Nesneleri 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sınıf_Nesneleri_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account();
+            var result = acc.GetAccount(3);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
