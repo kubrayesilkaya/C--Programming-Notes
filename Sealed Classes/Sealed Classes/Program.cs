dfd7d4374af77bb9569009de3d7493d71205b776
Kübra Yeşilkaya <93487264+kubrayesilkaya@users.noreply.github.com>
2023-08-28 15:49:50 +0300

    Sealed Classes

---
+++ Sealed Classes/Sealed Classes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sealed_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sealed Classes (Mühürlü Sınıf)
+
+            // Sealed bir sınıfı mühürler ve o sınıftan miras alınmasını engeller.
+
+            // Başına sealed yazılan class'tan miras alınamaz.
+
+            B b = new B();
+            // b.PropA = 1; // yapılamaz, çünkü a'dan miras alınamaz, a sealed class olması nedeni ile.
+            b.PropB = 3;
+
+            Console.ReadLine();
+        }
+
+        sealed class A
+        {
+            public int PropA { get; set; }
+        }
+
+        class B // : A // hata verir, a'dan miras alınamaz çünkü sealed class!
+        {
+            public int PropB { get; set;}
+        }
+    }
+}
