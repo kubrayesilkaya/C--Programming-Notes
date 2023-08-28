4b8240fe421415b426105711c48ac654fdf683ec
Kübra Yeşilkaya <93487264+kubrayesilkaya@users.noreply.github.com>
2023-08-28 11:34:51 +0300

    Inheritance

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inheritance (Kalıtım / Miras)
+
+            A a = new A();
+            a.PropA = 3;
+
+            B b = new B();
+            b.PropB = 5;
+            b.PropA = 8;
+
+            C c = new C();
+            c.PropC = 1;
+
+            Console.ReadLine();
+            
+        }
+    }
+
+    class A          // Base (Temel Sınıf)
+    {
+        public int PropA { get; set; }
+    }
+
+    class B : A     // Derived, türemiş sınıf
+    {
+        public int PropB { get; set; }
+    }
+
+    class C : B
+    {
+        public int PropC { get; set; }
+    }
+}
