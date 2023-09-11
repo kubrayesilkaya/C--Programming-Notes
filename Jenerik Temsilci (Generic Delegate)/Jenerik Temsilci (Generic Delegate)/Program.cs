2407fa2b9655d5ee3200626f7df04ee33dae8c58
Kübra Yeşilkaya <93487264+kubrayesilkaya@users.noreply.github.com>
2023-09-11 12:35:24 +0300

    Jenerik Temsilci (Generic Delegate)

---
+++ Jenerik Temsilci (Generic Delegate)/Jenerik Temsilci (Generic Delegate)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jenerik_Temsilci__Generic_Delegate_
+{
+    internal class Program
+    {
+        // Metotlar void olduğu için delegate'in de void olması gerekiyor.
+        delegate void MyGenericDelegate<T>(T args);
+
+        static void Main(string[] args)
+        {
+            // Temsilciler (Delegate)
+
+            MyGenericDelegate<string> stringDelegate = new MyGenericDelegate<string>(StringTarget);
+            stringDelegate("Kind World");
+
+            MyGenericDelegate<int> intDelegate = new MyGenericDelegate<int>(IntTarget);
+            intDelegate(1);
+
+            Console.ReadLine();
+        }
+
+        static void StringTarget(string arg)
+        {
+            Console.WriteLine("Arg metnini büyüt : " + arg.ToUpper());
+        }
+
+        static void IntTarget(int arg)
+        {
+            Console.WriteLine("++arg : " + ++arg);
+        }
+    }
+}
---
+++ Jenerik Temsilci (Generic Delegate)/Jenerik Temsilci (Generic Delegate)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Jenerik Temsilci (Generic Delegate)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Jenerik Temsilci (Generic Delegate)")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("669462d3-261b-446e-8e11-e533dc1fa350")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
