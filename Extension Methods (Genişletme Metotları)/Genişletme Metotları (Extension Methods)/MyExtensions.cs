4c092c2bce3490a303e1b6ae4d298a906b784847
Kübra Yeşilkaya <93487264+kubrayesilkaya@users.noreply.github.com>
2023-09-11 16:32:15 +0300

    Extension Methods (Genişletme Metotları)

---
+++ Extension Methods (Genişletme Metotları)/Genişletme Metotları (Extension Methods)/MyExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genişletme_Metotları__Extension_Methods_
+{
+    public static class MyExtensions
+    {
+        public static void DisplayASMInfo(this object obj) 
+        {
+            Console.WriteLine("Object Type : {0}, Assembly : \n", obj.GetType().Name,
+                Assembly.GetAssembly(obj.GetType()).GetName().Name);
+        }
+
+        /// <summary>
+        /// Projedeki her int veri tipi için kullanılabilir olan "karesini alma" metodudur.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+
+        public static int Square(this int i)
+        {
+            return i * i;
+        }
+
+        public static void ShowItems(this System.Collections.IEnumerable iterator)
+        {
+            foreach (var item in iterator)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Artık bir liste oluşturduğumuzda, herhangi bir generic liste (Collections), ShowItems metoduna sahip olacak,
+            // ve bunu çağırdığımız zaman otomatik olarak o item içerisindeki tüm veriyi listeleyecek ekranda. 
+        }
+    }
+}
---
+++ Extension Methods (Genişletme Metotları)/Genişletme Metotları (Extension Methods)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genişletme_Metotları__Extension_Methods_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Extension Methods (Genişletme Metotları)
+
+            object obj1 = new object();
+            obj1 = new Program();
+            obj1.DisplayASMInfo();
+
+            int x = 5;
+            Console.WriteLine(x.Square());
+
+            ArrayList list = new ArrayList();
+            list.Add("Adil");
+            list.Add("Erdem");
+            list.ShowItems();
+
+            Console.ReadLine();
+        }
+    }
+}
