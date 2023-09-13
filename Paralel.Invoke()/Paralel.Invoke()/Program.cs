ab2382afd990f28637df2c95add228e94d96cc28
Kübra Yeşilkaya <93487264+kubrayesilkaya@users.noreply.github.com>
2023-09-13 16:07:16 +0300

    Paralel.Invoke()

---
+++ Paralel.Invoke()/Paralel.Invoke()/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Paralel.Invoke__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parallel.Invoke() birden fazla metodu parametre olarak alır ve bunların hepsini paralel bir şekilde çalıştırır.
+
+            Console.WriteLine("Main thread başladı.");
+
+            Thread.Sleep(2000);
+
+            Parallel.Invoke(My2, My1);
+
+            Console.WriteLine("Main thread bitti.");
+
+            Console.ReadLine();
+        }
+
+        static void My1()
+        {
+            Console.WriteLine("My1 başladı.");
+
+            for(int count = 0; count < 5; count++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("My1 count is : " + count);
+            }
+
+            Console.WriteLine("My1 bitti.");
+        }
+
+        static void My2()
+        {
+            Console.WriteLine("My2 başladı.");
+
+            for(int count = 0;count < 5; count++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("My2 count is : " + count);
+            }
+
+            Console.WriteLine("My2 bitti.");
+        }
+    }
+}
