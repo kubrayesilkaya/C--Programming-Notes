aebd3cd7e207cb026358782f043599805460b1e9
Kübra Yeşilkaya <93487264+kubrayesilkaya@users.noreply.github.com>
2023-09-13 11:56:31 +0300

    Thread Nesnesi Özellikleri

---
+++ Thread Nesnesi Özellikleri/Thread Nesnesi Özellikleri/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread_Nesnesi_Özellikleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             IsAlive     Thread'in çalışma durumunu verir.
+             Name        Thread'in adını atar ya da verir.
+             Priority    Thread'in çalışma önceliğini atar ya da verir.
+             Abort       ThreadAbortException hatasını atarak thread'in çalışmasını iptal eder.
+             Resume      Beklemeye geçirilmiş thread'in çalışmasının devam etmesini sağlar.
+             Sleep       Belirtilen süre kadar beklemeye geçer.
+             Suspend     Thread'i geçici süre durdurur.
+             */
+        }
+    }
+}
