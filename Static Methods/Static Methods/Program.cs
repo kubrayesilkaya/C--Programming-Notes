349760947ec57b1291364e36cf87188d38f257e4
Kübra Yeşilkaya <93487264+kubrayesilkaya@users.noreply.github.com>
2023-09-27 11:39:03 +0300

    Static Methods

---
+++ Static Methods/Static Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sonuc = HelperMethods.KarakterDuzelt("ölçme ve değerlendirme");
+            Console.WriteLine(sonuc);
+
+            Console.ReadLine();
+        }
+    }
+
+    class HelperMethods
+    {
+        public static string KarakterDuzelt(string str)
+        {
+            return
+                str.Replace("ö", "o")
+                .Replace("ü", "u")
+                .Replace("ı", "i")
+                .Replace("ğ", "g")
+                .Replace("ç", "c")
+                .Replace(" ", "-");
+        }
+    }
+}
