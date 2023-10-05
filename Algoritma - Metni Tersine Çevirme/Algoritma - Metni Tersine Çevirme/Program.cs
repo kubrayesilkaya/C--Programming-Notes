012383d5d85cef9a8da872582a5fbc2e36089ea2
Kübra Yeşilkaya <93487264+kubrayesilkaya@users.noreply.github.com>
2023-10-05 13:02:19 +0300

    Algoritma - Metni Tersine Çevirme

---
+++ Algoritma - Metni Tersine Çevirme/Algoritma - Metni Tersine Çevirme/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma___Metni_Tersine_Çevirme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Algoritma : Metni Tersine Çevirme
+
+            // Yağmur
+            // ruğaY
+
+            var result = Reverse("yağmur");
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+
+        public static string Reverse(string data)
+        {
+            string result = "";
+
+            for(int i = data.Length - 1; i >= 0; i--)
+            {
+                result += data[i];
+            }
+
+            return result;
+        }
+    }
+}
